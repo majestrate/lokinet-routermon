22ce06a479839305f19c4c20d5f69cbaeeff71c2
Jeff Becker <jeff@i2p.rocks>
2021-04-30 14:17:09 -0400

    bundle socketiojs

--- static/app.js
+++ static/app.js
@@ -1,3 +1,12 @@
 /** app.js 
     fill me up with logic uwu
 */
+
+
+const createApp = () => {
+
+  let root = document.getElementById("root");
+  return (data) => {
+    root.innerText = JSON.stringify(data);
+  };
+};

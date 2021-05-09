9625bed2bd53643614d9fbadfb89095fd492a7b7
Jeff Becker <jeff@i2p.rocks>
2021-05-09 12:19:05 -0400

    again

--- static/app.js
+++ static/app.js
@@ -39,7 +39,7 @@ const updateNodes = (svg, inbound, outbound, samples, config) => {
   
   let x_pos = 10;
   const width = config.width || 10;
-  let y_pos = 10;
+  let y_pos = 20;
   const height = config.heigth || window.innerHeight;
 
   const history = config.history || 30;
